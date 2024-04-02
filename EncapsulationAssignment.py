3b44a868331f01c7d8da23f3b63e1b494dfe23d8
maesiman <164886269+maesiman@users.noreply.github.com>
2024-04-02 08:55:21 +0800

    Added comments

--- EncapsulationAssignment.py
+++ EncapsulationAssignment.py
@@ -2,14 +2,17 @@
 
 
 class Protected:
+    # initialize protected and private attributes
     def __init__(self):
         self._protectedFName = "Unknown Fname"
         self.__privateLName = "Unknown Lname"
 
+     # protected function
     def setProtectedFName(self, protected):
         self._protectedFName = protected
         return self._protectedFName
 
+    # private function
     def setPrivateLName(self, private):
         self.__privateLName = private
         return self.__privateLName
